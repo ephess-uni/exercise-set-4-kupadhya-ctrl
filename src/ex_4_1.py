81992d4a8d4f3640062b4d838893e62b10ce6cfd
kupadhya-ctrl <149606458+kupadhya-ctrl@users.noreply.github.com>
2023-12-09 07:01:52 +0530

    Update2 ex_4_1.py

--- src/ex_4_1.py
+++ src/ex_4_1.py
@@ -14,27 +14,31 @@ FILENAME = get_data_file_path('messages.log')
 
 
 def num_shutdowns(logfile):
-     """
+    """
     Counts and returns the number of shutdowns present in the file.
-    
+
     Args:
         logfile (str): The path to the log file.
 
     Returns:
         int: The number of shutdown events present in the file.
     """
-     # Use the get_shutdown_events function from ex_4_0 to get shutdown entries
-     
-     shutdown_entries = get_shutdown_events(logfile)
-     
-    
+    # Use the get_shutdown_events function from ex_4_0 to get shutdown entries
+    shutdown_entries = get_shutdown_events(logfile)
 
-     # Count the number of shutdown events (each event has two entries)
-     
-     num_shutdowns = len(shutdown_entries) // 2
-     
+    # Initialize a counter for the number of shutdown events
+    num_shutdowns = 0
 
-     return num_shutdowns
+    # Iterate through the shutdown entries and count pairs of "Shutdown initiated" and "Shutdown complete"
+    i = 0
+    while i < len(shutdown_entries):
+        if shutdown_entries[i] == "Shutdown initiated" and i + 1 < len(shutdown_entries) and shutdown_entries[i + 1] == "Shutdown complete":
+            num_shutdowns += 1
+            i += 2  # Move to the next pair
+        else:
+            i += 1  # Move to the next entry
+
+    return num_shutdowns
 
 
 # >>>> The code below will call your function and print the results
