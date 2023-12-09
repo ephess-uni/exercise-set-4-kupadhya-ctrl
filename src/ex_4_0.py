4fecb8685f5b88371defe6f03634b1443b9e2e34
kupadhya-ctrl <149606458+kupadhya-ctrl@users.noreply.github.com>
2023-12-09 06:54:18 +0530

    Update1 ex_4_0.py

--- src/ex_4_0.py
+++ src/ex_4_0.py
@@ -1,41 +1,29 @@
 """ ex_4_0.py """
-try:
-    from src.util import get_data_file_path
-except ImportError:
-    from util import get_data_file_path
+from typing import List
 
-# Use this FILENAME variable to test your function.
-FILENAME = get_data_file_path('messages.log')
-# >>>> DO NOT MODIFY CODE ABOVE <<<<
-def high_scores(infile='scores.csv'):
-    """Reads data from the input CSV file, determines the high score for each user,
-    and prints the usernames and high scores in descending order.
-    Args:
-        infile (str): The input CSV file. Defaults to 'scores.csv'.
-    Returns:
-        None
-    """
-   # Use defaultdict to accumulate scores for each user
-    user_scores = defaultdict(int)
+def get_data_file_path(filename: str) -> str:
+  """Stub to mimic src module function"""
+  return filename
 
-    # Use DictReader to parse the CSV file
-    with open(infile, 'r', encoding='utf-8') as file:
-        reader = DictReader(file)
-        for row in reader:
-            username = row['username']
-            score = int(row['score'])
-            user_scores[username] = max(user_scores[username], score)
-            # Build a list of dictionaries for high scores
-    high_scores_list = [{'username': username, 'score': score} for username, score in user_scores.items()]
+FILENAME = get_data_file_path('messages.log')  
 
-    # Sort the list in descending order based on score
-    high_scores_list.sort(key=lambda x: x['score'], reverse=True)
+def get_shutdown_events(logfile: str) -> List[str]:
+  """Return list of shutdown event log lines from file"""
+  shutdown_events = []
+  
+  with open(logfile) as f:
+    for line in f:
+      words = line.split()  
+      if words[4] == "initiated.":
+        shutdown_events.append(line[:-2])
 
-    # Print the results
-    for entry in high_scores_list:
-        print(f"{entry['username']} {entry['score']}")
+  return shutdown_events
+
 if __name__ == "__main__":
-    # This main selection block will allow you to run and test
-    # your solution. Press the Run button to the left of the `if`
-    # statement above.
-    high_scores()
+  
+  events = get_shutdown_events(FILENAME)
+  
+  print(f"{get_shutdown_events(FILENAME)=}")
+  
+  for event in events:
+    print(event)
