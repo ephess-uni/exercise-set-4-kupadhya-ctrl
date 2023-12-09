30403b173df1c0c3b602f31eddd8d86a7557aa86
kupadhya-ctrl <149606458+kupadhya-ctrl@users.noreply.github.com>
2023-12-09 07:37:16 +0530

    Update ex_4_1.py

--- src/ex_4_1.py
+++ src/ex_4_1.py
@@ -1,28 +1,25 @@
+""" ex_4_1.py """
 import os
 from src.ex_4_0 import get_shutdown_events
-from src.ex_4_2 import logstamp_to_datetime
 from src.util import get_data_file_path
 
 # Use this FILENAME variable to test your function.
-FILENAME = get_data_file_path("messages.log")
+FILENAME = get_data_file_path('messages.log')
 
-def time_between_shutdowns(logfile):
+def num_shutdowns(logfile):
     """
-    Calculate the time between the first and last shutdown events in the given log file.
+    Count and return the number of shutdown events in the given log file.
 
     Parameters:
     - logfile (str): The path to the log file.
 
     Returns:
-    - timedelta: The time difference between the first and last shutdown events.
+    - int: The number of shutdown events in the log file.
     """
     shutdown_events = get_shutdown_events(logfile)
-
-    timestamps = [logstamp_to_datetime(event.split()[1]) for event in shutdown_events]
-
-    return timestamps[-1] - timestamps[0]
+    return len(shutdown_events)
 
 # The code below will call your function and print the results
 if __name__ == "__main__":
-    result = time_between_shutdowns(FILENAME)
-    print(f'Time between shutdowns: {result}')
+    shutdown_count = num_shutdowns(FILENAME)
+    print(f'Number of shutdown events: {shutdown_count}')
